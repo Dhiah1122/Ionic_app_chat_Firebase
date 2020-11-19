508baffb57ddb0eb0f75cf1adcc9ce2e28f2f9ec
Dhia Hannachi <dhia.hannachi234@gmail.com>
2020-11-19 13:28:51 +0100

    Chat app with firebase and ionic

---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyBklQIjxZi_xjzY2Ik1594KIpyo0UnNuF0",
+    authDomain: "chationicfb-f8b6d.firebaseapp.com",
+    databaseURL: "https://chationicfb-f8b6d.firebaseio.com",
+    projectId: "chationicfb-f8b6d",
+    storageBucket: "chationicfb-f8b6d.appspot.com",
+    messagingSenderId: "1098741266950",
+    appId: "1:1098741266950:web:4eb4a757413c9d3fe26187",
+    measurementId: "G-R6KL6QPPY1"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
